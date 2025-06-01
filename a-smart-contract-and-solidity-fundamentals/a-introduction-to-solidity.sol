8e506bdef4bf1ce1e0451f9a6e9ae85277dfa246
Lemuel <alvinwsauls@gmail.com>
2025-06-01 20:00:56 +0200

    visibility and constants and immuatbility

--- a-smart-contract-and-solidity-fundamentals/a-introduction-to-solidity.sol
+++ a-smart-contract-and-solidity-fundamentals/a-introduction-to-solidity.sol
@@ -59,3 +59,57 @@ uint256 private password = 123456; // other contracts can't access
 uin256 internal sharedSecret = 35; // visible to this contract and child contracts
 
 // Please note: the default visibility is "internal" if not specified
+
+// Constant And Immutable Variables
+
+//  cnostant and immutable variables reduce gs costs and improves security.
+
+// Constant Variables: vconstant variables must be assigned at declaration (caanot be altered afterwards).
+
+contract TokenContract {
+    // must be assigned at declaration
+    uint256 public constant DECIMAL_PLACES = 18;
+    string public constant TOKEN_NAME = "My Token";
+    address public constant DEAD_ADDRESS = 0x000000000000000000000000000000000000dEaD;
+}
+
+// Characteristics of Constant variables:
+// a value must be assigned when declared
+// value set at compile time
+// cannot be altered after contract deployment
+// do not take up storage slots (saving gas)
+// only value types and strings can be constants
+// constant variables are usually CAPITALIZED/UPPERCASED
+
+// Immutable Variables: Variables amreked as immutable can be assigned only once, but this assignment takes place in the constructor
+ contract TokenContract {
+    // declared but not assigned a value yet;
+    // immutable variables may be uninitialisied at this point
+    address public immutable deployer;
+    uint256 public immutable deploymentTime;
+
+    constructor() {
+        // Assigned once in the constructor
+        deployer = msg.sender;
+        deploymentTime = block.timestamp;
+    }
+ }
+// Characterisics of immutable variables
+// * can be assigned in the constructor or at decalration
+// * cannot be changed afterwards construction
+// * more gas efficient than regular state variables
+// * less gas efficient than constants
+// * only value types can be immutable (not strings or reference types)
+
+
+// When to use each:
+// * use constant for values known at compile time (eg. mathematical
+//    constants, configurable values)
+// Use immutable for values that depend on deployment conditions but won't be changed
+// after that (e.g. deployer address, configuration based on constructor arguments)
+// use regular state variables for values that need to change during the contract's lifetime
+
+// Use constant and immutable whenever possible to:
+// reduce gas costs.
+// makes contract intentions clearer.
+// improves security by preventing accidental state changes.
